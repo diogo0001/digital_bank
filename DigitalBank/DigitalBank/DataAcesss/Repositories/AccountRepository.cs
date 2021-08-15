e487453cac378cc4118913377625f13dff6c0278
diogo <diogo.t0001@gmail.com>
2021-08-15 17:50:55 -0300

    Basic repositories

---
+++ DigitalBank/DigitalBank/DataAcesss/Repositories/AccountRepository.cs
@@ -0,0 +1,68 @@
+﻿using DigitalBank.DataAcesss.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace DigitalBank.DataAcesss.Repositories
+{
+    public class AccountRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public AccountRepository(AppDbContext AppDbContext)
+        {
+            _appDbContext = AppDbContext;
+        }
+
+        // sacar
+        public Account TakeValueAwayByAccountNumber(int accountNumber, int takeAwaiValue)
+        {
+            var Account = _appDbContext.Account
+                .Include(e => e.User)
+                .Where(e => e.AccountNumber == accountNumber)
+                .FirstOrDefault();
+
+            if (Account != null) {
+                return Account;
+            }
+
+            return null;
+        }
+
+        // depositar
+        public Account DepositValueByAccountNumber(int accountNumber, int takeAwaiValue)
+        {
+            var Account = _appDbContext.Account
+                .Include(e => e.User)
+                .Where(e => e.AccountNumber == accountNumber)
+                .FirstOrDefault();
+
+            if (Account != null)
+            {
+                return Account;
+            }
+
+            return null;
+        }
+
+        // saldo
+        public Account GetAccountDataByAccountNumber(int accountNumber)
+        {
+            var Account = _appDbContext.Account
+                .Include(e => e.User)
+                .Where(e => e.AccountNumber == accountNumber)
+                .FirstOrDefault();
+
+            if (Account != null)
+            {
+                return Account;
+            }
+
+            return null;
+        }
+
+    }
+}
---
+++ DigitalBank/DigitalBank/DataAcesss/Repositories/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using DigitalBank.DataAcesss.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalBank.DataAcesss.Repositories
+{
+    public class UserRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public UserRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public List<User> GetUsers()
+        {
+            return _appDbContext.User.ToList();
+        }
+
+        public List<User> GetUsersWithAccounts()
+        {
+            return _appDbContext.User
+                .Include(e => e.Accounts)
+                .ToList();
+        }
+    }
+}
