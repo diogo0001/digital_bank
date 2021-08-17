2d00b9459cb939477ac2da02bbf934c2330e3f5b
diogo <diogo.t0001@gmail.com>
2021-08-16 22:21:54 -0300

    DB insert scripts

---
+++ DB/Insert_account_in_db.sql
@@ -0,0 +1,10 @@
+INSERT INTO Account (AccountNumber, AccountValue, UserId)
+VALUES 
+(2547,56,1),
+(2578,220,2),
+(2589,15,3),
+(2532,1458,4),
+(2548,325,5),
+(2576,0,6)
+
+SELECT * FROM Account
---
+++ DB/Insert_user_in_db.sql
@@ -0,0 +1,10 @@
+INSERT INTO [User] (Name,Email,CPF)
+VALUES 
+('Ana Maria','ana@gmail.com','123.456.789-98'),
+('João Alberto','joao@gmail.com','123.456.789-56'),
+('Roberto Costa','beto@gmail.com','123.456.789-15'),
+('Paulo Zezo','paulo@gmail.com','123.456.789-32'),
+('Caco Vilela','caco@gmail.com','123.456.789-10'),
+('Luna Sol','luna@gmail.com','123.456.789-49')
+
+SELECT * FROM[User]
